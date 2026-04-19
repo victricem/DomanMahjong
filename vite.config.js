a7ad37f76063a578f6b492d42e29081c971b2842
Victrricem <35552149+victricem@users.noreply.github.com>
2026-04-19 22:37:20 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // 重要：請替換成你的 Repository 名稱，前後都要有斜線
-  base: '/catcafe-mahjong/', 
+  // 這裡必須跟你的 GitHub Repository 名稱完全一模一樣！大小寫也要對！
+  base: '/DomanMahjong/', 
   plugins: [react()],
 })
